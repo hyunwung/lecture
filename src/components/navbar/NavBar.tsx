1b2dc4f90eef41cd702c61c536cb2c0c211cc123
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-12-11 11:22:27 +0900

    FEAT 초기 셋팅

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./NavBar.scss"
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+
+    </div>
+  )
+}
+
+export default NavBar
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from '../components/header/Header'
+import NavBar from '../components/navbar/NavBar'
 
 const Main = () => {
   return (
     <div className='main'>
-        
+      <Header></Header>
+      <NavBar></NavBar>
     </div>
   )
 }
